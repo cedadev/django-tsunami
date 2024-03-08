081f08ab6354c517543f685df2d11fd89508fbf3
Alex Manning <alexander.manning@stfc.ac.uk>
2024-03-08 10:01:17 +0000

    Remove deprecated use of pkg_resources.

--- tsunami/__init__.py
+++ tsunami/__init__.py
@@ -1,7 +1,2 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass
 
 default_app_config = __name__ + '.apps.TsunamiAppConfig'
